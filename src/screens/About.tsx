0f38da62669b7430954cd787c26574693be0efb5
markusan6085A <markusan6085@gmail.com>
2026-01-18 10:54:38 +0300

    Fix broken Cyrillic text in About.tsx

--- src/screens/About.tsx
+++ src/screens/About.tsx
@@ -1,4 +1,4 @@
-﻿import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { getOnlinePlayers } from "../utils/api";
 
 type Navigate = (p: string) => void;
