4583f42dc86f4841eaec496b91ab705d2e4d52c8
markusan6085A <markusan6085@gmail.com>
2026-01-18 11:06:12 +0300

    Recreate About.tsx - fix encoding issue

--- src/screens/About.tsx
+++ src/screens/About.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+﻿import React, { useState, useEffect } from "react";
 import { getOnlinePlayers } from "../utils/api";
 
 type Navigate = (p: string) => void;
